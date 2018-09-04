b167cc04f2edb7623b02730d801f7c88d1540683
Robert Varga <robert.varga@pantheon.tech>
2018-09-04 23:36:44 +0200

    Add FailedNodeTest
    This updates code coverage of FailedNode.
    
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ triemap/src/test/java/tech/pantheon/triemap/FailedNodeTest.java
@@ -0,0 +1,52 @@
+/*
+ * (C) Copyright 2018 Pantheon Technologies, s.r.o. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tech.pantheon.triemap;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FailedNodeTest {
+    @Mock
+    private MainNode<Object, Object> main;
+
+    private FailedNode<Object, Object> failed;
+
+    @Before
+    public void before() {
+        failed = new FailedNode<>(main);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testSize() {
+        failed.size(null);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testTrySize() {
+        failed.trySize();
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("FailedNode(" + main + ")", failed.toString());
+    }
+}
