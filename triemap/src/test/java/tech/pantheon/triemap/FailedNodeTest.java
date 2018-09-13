c7c327424d7e42e3e9f8a0e5fa7ed62822798d58
Robert Varga <robert.varga@pantheon.tech>
2018-09-13 19:24:17 +0200

    Bump to odlparent-4.0.0
    This bumps us to use odlparent-4.0.0, along with updates to accomodate
    mockito upgrade.
    
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- triemap/src/test/java/tech/pantheon/triemap/FailedNodeTest.java
+++ triemap/src/test/java/tech/pantheon/triemap/FailedNodeTest.java
@@ -21,9 +21,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
 public class FailedNodeTest {
     @Mock
     private MainNode<Object, Object> main;
